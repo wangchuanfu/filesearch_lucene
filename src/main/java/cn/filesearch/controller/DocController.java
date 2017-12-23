6ebafff96b876ba055fb8c4884a31ad62c2836f4
napoay <1965678754@qq.com>
2017-12-23 15:38:28 +0800

    update index

--- src/main/java/cn/filesearch/controller/DocController.java
+++ src/main/java/cn/filesearch/controller/DocController.java
@@ -15,12 +15,10 @@ import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.search.highlight.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.util.HtmlUtils;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
